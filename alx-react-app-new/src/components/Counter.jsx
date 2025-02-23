1367d3a19c70440aed10b0726f5cd4022378f018
Jumah18 <jumahokiki18@gmail.com>
2025-02-23 19:00:38 +0100

    hj

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -8,8 +8,8 @@ function Counter(){
         <>
         <div>
             <p>Current count is {count}</p>
-            <button onClick={() => setCount (count + 1)} style={{ fontWeight: "bolder", fontSize: '20px'}}>+ </button>
-            <button onClick={() => setCount (count- 1 )} style={{fontWeight : "bolder", fontSize : '20px'}}>- </button>
+            <button onClick={() => setCount (count + 1)} style={{ fontWeight: "bolder", fontSize: '20px'}}>Increment</button>
+            <button onClick={() => setCount (count- 1 )} style={{fontWeight : "bolder", fontSize : '20px'}}>Decrement</button>
             <button onClick={() => setCount(0)} style={{ fontWeight: "bolder", fontSize: '20px'}}>Reset</button>
         </div>
         </>
