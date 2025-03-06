ef27a2c778d805ec66b2723a4b5dc12b6fc1f77b
Jumah18 <jumahokiki18@gmail.com>
2025-03-06 20:02:51 +0100

    m

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,30 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Recipe Manager</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold mb-4">Recipe Manager</h1>
+        <Link to="/" className="mr-4 text-blue-500">Home</Link>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
