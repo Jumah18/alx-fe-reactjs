bd7be9ed80f271318835634ead711d69a0ef42c1
Jumah18 <jumahokiki18@gmail.com>
2025-03-12 12:17:18 +0100

    j

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,8 @@ import Massey from './Massey.png'
 function UserProfile() {
     return (
         <>
-      <div className="user-profile bg-gray-100 p-8 max-w-[400] max-auto my-20 rounded-1g shadow-lg" >
+       
+      <div className="user-profile bg-gray-100 p-8 max-w-sm max-auto rounded-1g  my-20 shadow-lg" >
         <img src={Massey} alt="User" className='w-36 h-36 rounded-full max-auto'/>
         <h1 className='text-xxl text-blue-800 my-4'>John Doe</h1>
         <p className='text-gray-600 text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
