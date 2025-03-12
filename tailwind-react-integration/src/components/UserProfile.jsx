7399bb66520ae432e16e35378af639ada43bfd89
Jumah18 <jumahokiki18@gmail.com>
2025-03-12 12:52:55 +0100

    j

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
         <>
        
       <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto rounded-lg  my-20 shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm sm:text-lg md:text-xl sm:text-sm md:text-base " >
-        <img src={Massey} alt="User" className='w-36 h-36 rounded-full max-auto sm:w-24 sm:h-24 md:w-26 md:h-26'/>
+        <img src={Massey} alt="User" className='w-36 h-36 rounded-full max-auto sm:w-24 sm:h-24 md:w-36 md:h-36'/>
         <h1 className='text-xl text-blue-800 my-4'>John Doe</h1>
         <p className='text-gray-600 text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
